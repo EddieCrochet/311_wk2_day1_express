6d6a33c39c64c14df19bfe52df30f35c49ceaf53
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-09-02 19:29:19 -0500

    not much

--- index.js
+++ index.js
@@ -15,7 +15,18 @@ const { users } = require('./state')
 
 app.get("/users", (req, res) => {
   return res.json(users);
-})
+});
+
+/***
+ * * GET /users/1
+    * Give your server the ability to respond to a GET request with a path "/users/1"
+    *  and return the first user object from the users array from state.js
+    * we will use path parameters to achieve this
+ */
+
+ app.get("/user/:id", (req, res) => {
+  return res.json(users);
+});
 
 /* END - create routes here */
 
